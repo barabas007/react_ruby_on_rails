b2f7e324eff63c87e4d1a67c2364c65072a169d9
barabas007 <shop.ru.shop@mail.ru>
2021-04-24 17:58:36 +0300

    galleries

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Product, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
+  it { should have_many(:galleries)}
 
   it { should have_many(:related_products) }
   it { should have_many(:related) }
